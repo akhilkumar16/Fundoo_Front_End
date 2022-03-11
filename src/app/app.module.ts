2686aefcbf81349a80e5130a5198de0ee5e1462a
akhilkumar16 <akhilkumarchandragiri@gmail.com>
2022-03-11 14:10:23 +0530

    [Akhil] changes made in user pages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import {MatFormFieldModule} from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
